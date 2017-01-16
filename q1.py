2011915f9cab8c9b35f33ca1968e4152e5ebc53f
jakegsy <siyuangoh@gmail.com>
2017-01-17 06:38:29 +0800

    comms

--- q1.py
+++ q1.py
@@ -1,12 +1,23 @@
 import random as rand;
+import numpy as np;
 
 def ranFun(x):
     return 0.4345*x**3 - 5.607*x**2 + 16.78*x - 10.61
-x = []
-
+x = []; y = []; t = [];
 for i in range(0,50):
- x.append(ranFun(i*0.2) + rand.gauss(0,1));
+ x.append((i*0.2));
+ y.append(ranFun(i*0.2));
+ t.append(ranFun(i*0.2)+rand.gauss(0,1));
+ #create x with 50 points, y with functionalized points, t with random added.
+t_n = np.array(t)
+print t_n;print t_n.shape
+t_n.transpose;
+print t_n
 
-print x
+
+
+
+
+
 
 
