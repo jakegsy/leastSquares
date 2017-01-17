b883bf4d106f5b9656c84196f2a2875d9de4ddc4
jakegsy <siyuangoh@gmail.com>
2017-01-17 08:37:19 +0800

    update

--- q1.py
+++ q1.py
@@ -1,20 +1,43 @@
 import random as rand;
 import numpy as np;
+import matplotlib.pyplot as plt
 
 def ranFun(x):
     return 0.4345*x**3 - 5.607*x**2 + 16.78*x - 10.61
 x = []; y = []; t = [];
 for i in range(0,50):
- x.append((i*0.2));
+ x.append((i*0.2))
  y.append(ranFun(i*0.2));
  t.append(ranFun(i*0.2)+rand.gauss(0,1));
+
  #create x with 50 points, y with functionalized points, t with random added.
-t_n = np.array(t)
-print t_n;print t_n.shape
-t_n.transpose;
-print t_n
+t_row = np.asmatrix(t)
+t_col = t_row.transpose()
 
 
+def x_to_XX(input,m):
+    ls = []
+    xx = np.zeros(shape=(m,len(input)))
+    for i in range(len(input)):
+        for j in range(m):
+            xx[j,i] = input[i]**j
+    return np.asmatrix(xx)
+xx = x_to_XX(x,4)
+print xx.shape
+row = xx
+col = xx.transpose()
+xTxx = np.matmul(row,col)
+print xTxx.shape
+xTxx_inverse = np.linalg.inv(xTxx)
+xTxx_inverse_xT = np.matmul(xTxx_inverse, row)
+print xTxx_inverse_xT.shape
+w_1 = np.matmul(xTxx_inverse_xT,t_col)
+
+
+xx = x_to_XX(x,4).transpose()
+xxP = np.linalg.pinv(xx)
+w_2 = np.matmul(xxP,t_col)
+print w_2
 
 
 
